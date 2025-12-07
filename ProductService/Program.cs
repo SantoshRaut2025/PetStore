ebaa70dea290a614d4d2277b979a5ae4e440f49b
SantoshRaut2025 <oyeitscat@gmail.com>
2025-12-07 16:19:04 +0530

    added_redis_cache

--- ProductService/Program.cs
+++ ProductService/Program.cs
@@ -3,23 +3,26 @@ using ProductService.Dtos;
 using ProductService.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
-
 // Add services to the container.
-
-
-
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
-builder.Services.AddScoped<ProductService.Services.IProductService, ProductService.Services.ProductService>();
 builder.Services.AddDbContext<ProductService.Data.ProductDBContext>(options =>
 {
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddScoped<ProductService.Services.IProductService, ProductService.Services.ProductService>();
+
+
 builder.Services.AddControllers();
+builder.Services.AddMemoryCache();
+builder.Services.AddStackExchangeRedisCache(options =>
+{
+    options.Configuration = builder.Configuration.GetConnectionString("RedisConnection");
+    options.InstanceName = "ProductService_";
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
 
 builder.Services.AddAutoMapper(typeof(ProductProfile));
 
